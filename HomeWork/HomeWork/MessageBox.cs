72785c4c0247f65bcdea89319211f1678b5c7707
Yana-commits <74359419+Yana-commits@users.noreply.github.com>
2022-10-26 12:14:38 +0300

    Initial commit

---
+++ HomeWork/HomeWork/MessageBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class MessageBox
+    {
+        public event Action<State> action;
+        public async Task<bool> Open()
+        {
+            Console.WriteLine("Окно открыто");
+            await Task.Delay(3000);
+            Console.WriteLine("Окно закрыто");
+            action?.Invoke(StateMehtod());
+
+            return true;
+        }
+
+        public State StateMehtod()
+        {
+            State state = State.Cancel;
+            Random rnd = new Random();
+            int value = rnd.Next();
+
+            if (value % 2 == 0)
+            {
+                state = State.Ok;
+            }
+            return state;
+        }
+    }
+
+    public enum State
+    {
+        Ok,
+        Cancel
+    }
+}
---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+       async static Task Main(string[] args)
+        {
+            MessageBox messageBox = new MessageBox();
+
+            messageBox.action += CloseWindow;
+
+           await OpenAsync(messageBox);
+        }
+
+        public static void CloseWindow(State state)
+        {
+            Console.WriteLine($"{state}");
+        }
+        public static Task OpenAsync(MessageBox messageBox)
+        {
+            var task = new TaskCompletionSource<bool>();
+            Task.Run(async ()=> { 
+                bool result = await messageBox.Open();
+                task.SetResult(result);
+            });
+           
+
+            return task.Task;
+        }
+    }
+}
