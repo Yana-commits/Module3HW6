aa45e3e9895aa32fb77a0955411663d33bd9ad19
Yana-commits <74359419+Yana-commits@users.noreply.github.com>
2022-10-27 13:25:28 +0300

    second

--- HomeWork/HomeWork/MessageBox.cs
+++ HomeWork/HomeWork/MessageBox.cs
@@ -17,18 +17,6 @@ namespace HomeWork
             action?.DynamicInvoke();
         }
 
-        //public State StateMehtod()
-        //{
-        //    State state = State.Cancel;
-        //    Random rnd = new Random();
-        //    int value = rnd.Next();
-
-        //    if (value % 2 == 0)
-        //    {
-        //        state = State.Ok;
-        //    }
-        //    return state;
-        //}
     }
 
     public enum State
